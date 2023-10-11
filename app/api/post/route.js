08529463b8060157954d61f5bc4b943ebb2e01fb
Hernan Tapia <soliditize@MacBook-Pro.local>
2023-10-10 18:58:31 -0700

    added prisma client and API POST route for creating a new post

---
+++ app/api/post/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import prisma from "@/libs/prismadb";
+
+export const POST = async ( request ) => {
+
+    try {
+          const reqBody = await request.json();
+          const { title , content } = reqBody
+
+          const newPost = await prisma.post.create({
+            data: {
+              title,
+              content
+            }
+          })
+
+          return NextResponse.json(newPost);
+
+    } catch (error) {
+      return NextResponse.json({ message: 'POST error' , error } , { status: 500})
+    }
+}
